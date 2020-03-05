50e45434ad48202af799afd11aa0783278375af0
Ricky Orr <rlayneorr@gmail.com>
2020-03-05 13:42:28 -0500

    actually put in repository

---
+++ DiscoFever/src/test/java/com/richard/test/MyTest.java
@@ -0,0 +1,14 @@
+package com.richard.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyTest {
+	
+	@Test
+	public void test() {
+		assertEquals("True is true", true, true);
+	}
+
+}
